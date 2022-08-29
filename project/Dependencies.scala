3b0856d080349c1b73bd6f8da4993fc3cc1ead4e
Henrique Silva <henrique.rd.silva@gmail.com>
2022-08-29 17:24:38 +0100

    - support fifo and non-fifo sns topics - support for eventbridge

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -313,6 +313,7 @@ object Dependencies {
     "ecrpublic",
     "secretsmanager",
     "sns",
+    "eventbridge",
   ).map(artifactName => "software.amazon.awssdk" % artifactName % awsSdkV)
 
   private val googleCloudDependencies = List(
